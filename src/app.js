c0cb2d3249fda37d93b6334a4db3833b5b383d10
Joaquin Puente <joaquinpuentecoronel@gmail.com>
2024-03-04 21:40:48 -0300

    Fixed to railways

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const connection = mongoose.connect(process.env.MONGODB_URI)
 
 app.use(express.json());
 app.use(cookieParser());
